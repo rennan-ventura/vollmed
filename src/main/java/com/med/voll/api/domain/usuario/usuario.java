7b943d28ef08c4a2a4b76fcaf9463a97151a795f
Rennan Ventura <rennan.ventura.alves@gmail.com>
2023-11-12 22:42:48 -0300

    Mudando a migration

---
+++ src/main/java/com/med/voll/api/domain/usuario/usuario.java
@@ -0,0 +1,22 @@
+package com.med.voll.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+
+}
